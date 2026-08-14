7ea3cf146741dc51cf763e8f4a955a0234b20587
Victor Mark <innovimark@gmail.com>
2026-08-14 16:13:07 +0100

    ui for logs section

--- src/layout/PagesLayout.tsx
+++ src/layout/PagesLayout.tsx
@@ -20,7 +20,7 @@ const tabComponents: { [key: string]: React.ComponentType } = {
 };
 
 export default function PagesLayout() {
-  const [activeTab, setActiveTab] = useState("history");
+  const [activeTab, setActiveTab] = useState("log");
 
   const ActiveTab = tabComponents[activeTab];
   return (
--- src/pages/Log.tsx
+++ src/pages/Log.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useLogs } from "../context/LogContext/useLogs";
 
 export default function Log() {
+  const { logs } = useLogs();
+  console.log(logs);
   return (
-    <div>Log</div>
-  )
+    <div className="w-full flex flex-col bg-neutral-700 outline-neutral-600 px-4 py-5 sm:p-5 gap-5 rounded-2xl">
+      <div className="flex flex-col sm:flex-row justify-between items-center gap-2.5">
+        <p>Conversation log</p>
+        <div className="flex items-center gap-4 sm:justify-between">
+          <p className="text-5 text-neutral-50">{logs.length} logged</p>
+          <button className="uppercase bg-neutral-600 outline-1 outline-neutral-400 text-5 text-neutral-200 rounded-lg px-3 py-2 cursor-pointer hover:bg-neutral-400">clear all</button>
+        </div>
+      </div>
+    </div>
+  );
 }
