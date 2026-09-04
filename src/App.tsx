9005d9b57debdc428178cfc8d17607c15a7c5edb
Victor Mark <innovimark@gmail.com>
2026-09-04 16:01:03 +0100

    web app favicon updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import Nav from "./components/nav";
 import MarketTicker from "./components/carousel/MarketTicker";
 import CurrencyCard from "./components/CurrencyCard";
 import PagesLayout from "./layout/PagesLayout";
-import { useMarketRates } from "./hooks/useMarketRates";
+
 
 function App() {
-  const {data} = useMarketRates()
-  console.log(data)
 
   return (
     <div className="flex flex-col w-full h-dvh overflow-hidden gap-8 md:gap-12">
